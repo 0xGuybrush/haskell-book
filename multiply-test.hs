20a883b5135cf0596832bd74cc242020005fa0e3
Dave Moloney <whatsyourfavouritehummingnoise@gmail.com>
2018-05-06 18:48:33 +0100

    Chapter 2 exercises

---
+++ multiply-test.hs
@@ -0,0 +1,16 @@
+triple x = x * 3 
+
+multi = x * y
+    where x = 5
+          y = 6
+
+multi2 = x * 3 + y
+    where x = 3
+          y = 1000
+
+waxOn = x * 5
+    where z = 7
+          x = y ^ 2
+          y = z + 8
+
+waxOff x = triple x
