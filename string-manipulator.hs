b2f47713450c1e3f28e4dbfcf3f68e058f3fa5c2
Dave Moloney <whatsyourfavouritehummingnoise@gmail.com>
2018-05-13 18:50:14 +0100

    Word swap via take/drop

--- string-manipulator.hs
+++ string-manipulator.hs
@@ -8,3 +8,7 @@ fifthLetterOf sentence = sentence !! 4
 
 dropNine :: String -> String
 dropNine phrase = drop 9 phrase
+
+rvrs :: String -> String
+rvrs saying = dropNine saying ++ (take 4 (drop 5 saying))  ++ (take 5 saying) 
+
